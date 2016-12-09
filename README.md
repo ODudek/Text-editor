6a2b080777e463ceb4faeb58eaa0681e2ef36252
QuakeRPL <dudek.olek4@gmail.com>
2016-12-09 11:42:22 +0100

    first push

---
+++ README.md
@@ -0,0 +1,8 @@
+# Text editor
+
+Simple editor of text
+
+# How to use
+ 
+ 1. Choose text that you want to edit by your mouse
+ 2. Edit this text by choosing one of options from menu
---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let ARRAYOFHTML = [{
+    begin: ['<b>', '<i>', '<ins>', '<code>', '<mark>'],
+    end: ['</b>', '</i>', '</ins>', '</code>', '</mark>']
+}];
+
+let $text = document.getElementById('text');
+let selection = '';
+let $menu = document.getElementById('main-menu');
+let $clearText = $text.innerHTML;
+
+function selectedText() {
+    $text.addEventListener('mouseup', function () {
+
+        if (window.getSelection) {
+            selection = window.getSelection().toString();
+        } else if (document.selection) {
+            selection = document.selection.createRange().text;
+        }
+        return selection;
+    })
+
+}
+
+function clearEdit() {
+    $text.innerHTML = $clearText;
+    return $text;
+}
+
+function wrapString(selectedText, indexOfHtml) {
+    return ARRAYOFHTML[0].begin[indexOfHtml] + selectedText + ARRAYOFHTML[0].end[indexOfHtml];
+}
+
+function setupReplace(selectedText, indexOfHtml) {
+    let newText = wrapString(selectedText, indexOfHtml);
+    $text.innerHTML = $text.innerHTML.replace(selectedText, newText);
+}
+
+$menu.addEventListener('click', function (e) {
+    let chosenId = e.target.id;
+    if (chosenId == 'bold')
+        setupReplace(selection, 0);
+    else if (chosenId == 'italic')
+        setupReplace(selection, 1);
+    else if (chosenId == 'underlined')
+        setupReplace(selection, 2);
+    else if (chosenId == 'code')
+        setupReplace(selection, 3);
+    else if (chosenId == 'mark')
+        setupReplace(selection, 4);
+    else if (chosenId == 'clear')
+        clearEdit();
+});
+
+selectedText();
